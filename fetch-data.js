da68dabb43f1318e908184497c6cbd4b15f87a66
Wangui Githua <barb.wangui@gmail.com>
2025-06-14 18:23:42 -0400

    Fetch data using try catch

--- fetch-data.js
+++ fetch-data.js
@@ -4,5 +4,18 @@ async function fetchUserData(){
     const apiUrl = 'https://jsonplaceholder.typicode.com/users' ;
 
     // data container element
-    const dataContainer = document.getElementsById('api-data');
+    const dataContainer = document.getElementById('api-data');
+
+    try {
+        const response = await fetch(apiUrl);
+        
+        if (!response.ok){
+            throw new Error(`Response status: ${response.status}`);
+        }
+
+        const users = await response.json();
+        
+    } catch(){
+        
+    }
 }
