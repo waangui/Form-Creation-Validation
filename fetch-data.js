9655dac4de70122127cd17d22215eee284e1d854
Wangui Githua <barb.wangui@gmail.com>
2025-06-14 17:05:29 -0400

    Initialize async function, define API url, select data container element.

---
+++ fetch-data.js
@@ -0,0 +1,8 @@
+
+
+async function fetchUserData(){
+    const apiUrl = 'https://jsonplaceholder.typicode.com/users' ;
+
+    // data container element
+    const dataContainer = document.getElementsById('api-data');
+}
