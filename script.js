57ecb65a860252dff007e8d243eb820fc2b6e397
Wangui Githua <barb.wangui@gmail.com>
2025-06-14 15:32:14 -0400

    DOM content loaded, Form Submission Prevention, Retrieve username, email and password inputs

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+document.addEventListener('DOMContentLoaded', ()=> {
+  
+    const form = document.getElementById(id="registration-form");
+    const feedbackDiv = document.getElementById(id="form-feedback");
 
+    // Prevent form submission
+
+    form.addEventListener(submit, (event)=>{
+        event.preventDefault();
+    })
+
+    // Retrieve Input 
+
+    const userName = document.getElementById(id="username");
+    const emailInput = document.getElementById(id="email");
+    const passwordInpute = document.getElementById(id="password");
+
+})
